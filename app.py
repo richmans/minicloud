485108a184ba03868370b7996e8cd87effa2a10d
richmans <richmans@users.noreply.github.com>
2020-04-26 17:46:13 +0200

    quick demo of the docker control capabilities from python

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_restful import Resource, Api
 import docker
 
@@ -7,22 +7,33 @@ client = docker.from_env()
 app = Flask(__name__)
 api = Api(app)
 
-greetings = {'world': 'hello world'}
-
-class Greeting(Resource):
-    def get(self, greeting_id):
-        return greetings[greeting_id]
-
-    def put(self, greeting_id):
-        greetings[greeting_id] = request.form['greeting']
-        return {greeting_id: greetings[greeting_id]}
-
 class ContainerList(Resource):
     def get(self):
-        return [c.name for c in client.containers.list()]
+        return [{"name": c.name, "status": c.status} for c in client.containers.list(True) if 'minicloud' in c.name]
 
-api.add_resource(ContainerList, '/containers')
-api.add_resource(Greeting, '/<string:greeting_id>')
+class StopResource(Resource):
+    def get(self, container_name):
+        if not container_name.startswith("minicloud"):
+            abort("Invalid container name")
+        conts = [c for c in client.containers.list(True) if c.name == container_name]
+        if len(conts):
+            conts[0].stop()
+        else:
+            abort("Container not found")
+
+class StartResource(Resource):
+    def get(self, container_name):
+        if not container_name.startswith("minicloud"):
+            abort("Invalid container name")
+        conts = [c for c in client.containers.list(True) if c.name == container_name]
+        if len(conts):
+            conts[0].start()
+        else:
+            abort("Container not found")
+
+api.add_resource(ContainerList, '/')
+api.add_resource(StopResource, '/stop/<string:container_name>')
+api.add_resource(StartResource, '/start/<string:container_name>')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
